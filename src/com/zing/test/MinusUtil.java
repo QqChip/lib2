e6df2782b18f9108f0ca208d67a7e7eecfe1493b
QqChip <quyenpnq@gmail.com>
2022-04-18 15:38:52 +0700

    update

--- src/com/zing/test/MinusUtil.java
+++ src/com/zing/test/MinusUtil.java
@@ -13,4 +13,7 @@ public class MinusUtil {
     public static int minus(int a, int b){
         return a - b;
     }
+    public static int minus(int a, int b, int c){
+        return a - b - c;
+    }
 }
